d1953e1e78fb4168bb3866d81368e3acb481f70a
SRIKARTHOUTAM <srikarthoutam@gmail.com>
2016-07-29 19:12:10 -0400

    Add project files.

---
+++ EmployeeDetails/EmployeeFunctions.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ADO
+{
+    class Employee
+    {
+
+
+        public static void DisplayEmployees()
+        {
+
+
+            //retrive data fom database
+
+            //step 1 - open a connection to database
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=DESKTOP-CKBAAPJ;Initial Catalog=EmployeeDB;Integrated Security=True";
+            connection.Open();
+
+
+            //step 2 - run sql query
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "select * from Employee";
+            command.Connection = connection;
+
+
+            //step 3 - read data from result
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("{0} {1} {2}", reader["Id"], reader["FirstName"], reader["LastName"]);
+            }
+
+            connection.Close();
+            Console.ReadKey();
+        }
+
+        public static void DisplayEmployee()
+        {
+
+
+            //retrive data fom database
+
+            //step 1 - open a connection to database
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=DESKTOP-CKBAAPJ;Initial Catalog=EmployeeDB;Integrated Security=True";
+            connection.Open();
+
+
+            Console.Write("Enter Employee Id:");
+            string empId = Console.ReadLine();
+
+
+
+            //step 2 - run sql query
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "select * from Employee where Id = @id";
+            command.Connection = connection;
+
+            SqlParameter param1 = new SqlParameter("id", empId);
+            command.Parameters.Add(param1);
+
+
+            //step 3 - read data from result
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("{0} {1} {2}", reader["Id"], reader["FirstName"], reader["LastName"]);
+            }
+
+            connection.Close();
+            Console.ReadKey();
+        }
+
+        public static void InsertEmployee()
+        {
+            //step 1 - connect to database using sql connection, open connection
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=DESKTOP-CKBAAPJ;Initial Catalog=EmployeeDB;Integrated Security=True";
+            connection.Open();
+
+            Console.Write("Enter First Name :");
+            string fname = Console.ReadLine();
+
+            Console.Write("Enter Last Name :");
+            string lname = Console.ReadLine();
+
+            Console.Write("Enter Salary :");
+            decimal salary = decimal.Parse(Console.ReadLine());
+
+            //step 2 - run sql query
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "insert into Employee Values(@fname, @lname, @salary, getdate())";
+            command.Connection = connection;
+
+            SqlParameter param1 = new SqlParameter("fname", fname);
+            SqlParameter param2 = new SqlParameter("lname", lname);
+            SqlParameter param3 = new SqlParameter("salary", salary);
+
+            command.Parameters.Add(param1);
+            command.Parameters.Add(param2);
+            command.Parameters.Add(param3);
+
+            //step 3 - execute 
+            int status = command.ExecuteNonQuery();
+
+
+            Console.WriteLine("Row(s) effected: {0}", status);
+            connection.Close();
+
+            Console.ReadLine();
+
+        }
+
+        public static void DeleteEmployee()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=DESKTOP-CKBAAPJ;Initial Catalog=EmployeeDB;Integrated Security=True";
+            connection.Open();
+
+            Console.WriteLine("Enter the Employee ID to Delete:");
+            string empId = Console.ReadLine();
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "delete from Employee where Id = @id";
+            command.Connection = connection;
+
+            SqlParameter param1 = new SqlParameter("id", empId);
+            command.Parameters.Add(param1);
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{0} {1} {2}", reader["Id"], reader["FirstName"], reader["LastName"]);
+            }
+
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EmployeeDetails/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ConsoleApp_ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool flag = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Choose any Option from the list below");
+                Console.WriteLine("\t1.Insert Employee Details:");
+                Console.WriteLine("\t2.Display Employee Details:");
+                Console.WriteLine("\t3.Display All Employees:");
+                Console.WriteLine("\t4.Delete Employee:");
+                Console.WriteLine("\t5.Exit:");
+
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Employee.InsertEmployee();
+                        break;
+                    case "2":
+                        Employee.DisplayEmployee();
+                        break;
+                    case "3":
+                        Employee.DisplayEmployees();
+                        break;
+                    case "4":
+                        Employee.DeleteEmployee();
+                        break;
+                    case "5":
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Insert Valid Entry");
+                        break;
+                }
+
+
+            } while (flag);
+
+
+        }
+    }
+}
